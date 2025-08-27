a48e6d02c3ce10584ce53f8bc5ccbbb158c5c827
Michael <princemicare@gmail.com>
2025-08-27 05:15:54 +0100

    api error resolve

---
+++ src/pages/admin/PendingDeposits.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PendingDeposits = () => {
+  return (
+    <div>PendingDeposits</div>
+  )
+}
+
+export default PendingDeposits
---
+++ src/pages/public/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Aboutcontent from '../../components/Aboutcontent'
+
+const About = () => {
+  return (
+    <>
+
+    <Aboutcontent />
+    
+    </>
+  )
+}
+
+export default About
